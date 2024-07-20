1c126e9b5bba984bf37b447922e8b819af3ddef2
Vishwa Gunathilake <134719562+vishwa-prabhath@users.noreply.github.com>
2024-07-20 20:14:19 +0530

    Add files via upload
    6 files uploded .video 15 th eke updates ynne godak 16 idn mn hduwe 17 video eke thwa updates wgyk ynwa

---
+++ Throwables.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throwables : MonoBehaviour
+{
+    // Updates on throwables script
+
+
+// damage to enemy
+
+if (objectInRange.gameObject.GetComponent<Enemy>())
+{
+        objectInRange.gameObject.GetComponent<Enemy>().TakeDamage(100);
+}
+    
---
+++ Weapon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//edited texts in weapon cs
+
+
+public int weaponDamage;
+
+//fire method method
+
+Bullet bul = bullet.GetComponent<Bullet>();
+bul.bulletDamage = weaponDamage;
